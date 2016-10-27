35b189e99c3e739aeeee52b7198ade3b7cc19254
Joonas Jauhiainen2 <joonas.jauhiainen@eficode.com>
2016-10-27 20:33:40 +0300

    Updated project to support json api.

--- Startup.cs
+++ Startup.cs
@@ -1,19 +1,36 @@
-using System;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 using aspnetcoreapp.Models;
 
 namespace aspnetcoreapp
 {
+    
     public class Startup
     {
-        public void Configure(IApplicationBuilder app)
+        public IConfigurationRoot Configuration { get; }
+        public Startup(IHostingEnvironment env)
         {
-            app.Run(context =>
-            {
-                return context.Response.WriteAsync("Hello from ASP.NET Core!");
-            });
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+                .AddEnvironmentVariables();
+            Configuration = builder.Build();
+        }
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+            services.AddSingleton<ITodoRepository, TodoRepository>();
+        }
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
+            loggerFactory.AddDebug();
+
+            app.UseMvc();
         }
     }
 }
