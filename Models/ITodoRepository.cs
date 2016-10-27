b3550c396a327181faafe6e00126f3139dec046b
Joonas Jauhiainen2 <joonas.jauhiainen@eficode.com>
2016-10-27 18:27:22 +0300

    Starting working with project.

---
+++ Models/ITodoRepository.cs
@@ -0,0 +1,12 @@
+using System.Collections.Generic;
+
+namespace aspnetcoreapp
+{
+    public interface ITodoRepository {
+        void Add(TodoItem item);
+        IEnumerable<TodoItem> GetAll();
+        TodoItem Find(string key);
+        TodoItem Remove(string key);
+        void Update(TodoItem item);
+    }
+}
---
+++ Models/TodoItem.cs
@@ -0,0 +1,9 @@
+namespace aspnetcoreapp
+{
+    public class TodoItem {
+        public string Key {get; set;}
+        public string Name {get; set;}
+        public bool isComplete {get; set;}
+        
+    }
+}
